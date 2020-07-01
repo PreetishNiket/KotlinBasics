7d8d2f104a9c8e69ea9b27d08d8647b578693d63
Preetish Niket <niketpreetish@gmail.com>
2020-07-01 14:05:17 +0530

    Kotlin inheritance

---
+++ src/Inheritance.kt
@@ -0,0 +1,14 @@
+class Inheritance {
+    open inner class person()
+    {
+
+    }
+    inner class student :person()
+    {
+
+    }
+    fun main()
+    {
+        println("person is parent of student even if they both are inner class of inheritance name class")
+    }
+}

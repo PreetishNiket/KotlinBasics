612567f6eb07965bc1cc1c62a2313fb3b0e128bd
Preetish Niket <niketpreetish@gmail.com>
2020-08-06 23:14:01 +0530

    Basics

--- src/Extension Functions.kt
+++ src/Extension Functions.kt
@@ -3,4 +3,6 @@ fun Main.fullName()="${this.firstName} ${this.lastName}"
 fun main() {
     val main = Main("Jonas", "Kahnwald")
     println("The full name iof the student is:${main.fullName()}")
+    val arr= arrayOf(1,2,3)
+    val list= listOf(1,2,3)
 }

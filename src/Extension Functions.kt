a32d1ee516cb63cbfe5fd080be70febf4538530f
Preetish Niket <niketpreetish@gmail.com>
2020-08-06 23:48:16 +0530

    Basics

--- src/Extension Functions.kt
+++ src/Extension Functions.kt
@@ -1,5 +1,8 @@
 class Main(val firstName:String,val lastName:String)
 fun Main.fullName()="${this.firstName} ${this.lastName}"
+fun Int.triple(): Int { return this * 3 }
+var result = 3.triple()
+
 fun main() {
     val main = Main("Jonas", "Kahnwald")
     println("The full name iof the student is:${main.fullName()}")
--- src/Revision.kt
+++ src/Revision.kt
@@ -1,6 +1,11 @@
 class Revision {
+    lateinit var b:String
+    fun  main(){
+        b="0"
+    }
     val preet:String?=null
     val assk:String="Preetish"
     val sum =preet+assk
     val preetish =sum+assk
+
 }
